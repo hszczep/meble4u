30e5ed9c6adb8317da62cb6b5f0b61db03f23bcb
Hubert Szczepaniak <hszczepaniak@man.poznan.pl>
2025-06-12 00:43:58 +0200

    Updates

---
+++ assets/about-B1eMe8l1.js
@@ -0,0 +1,1 @@
+import{w as o,o as t}from"./chunk-NL6KNZEE-CHiMG1Zz.js";const s=o(function(){return t.jsx("div",{children:"about"})});export{s as default};
---
+++ assets/contact-YZCnEXLx.js
@@ -0,0 +1,1 @@
+import{w as t,o}from"./chunk-NL6KNZEE-CHiMG1Zz.js";const s=t(function(){return o.jsx("div",{children:"Contact"})});export{s as default};
